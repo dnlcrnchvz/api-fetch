ce2383d3205a499dc1dbaf315edd5595e2b9ff93
Daniela Ceron <dceronchavez09@gmail.com>
2024-10-15 20:43:02 -0600

    agregación de loader

--- script.js
+++ script.js
@@ -4,23 +4,30 @@ const rellenarCards = (usuarios, contenedorId) => {
     usuarios.forEach((usuario) => {
         const newCard = `
         <div class="container-card">
-            <img src="${usuario.avatar}" class="">
+            <img src="${usuario.avatar}" class="card-img-top">
             <div class="card-body">
                 <h1 class="card-title">${usuario.first_name}</h1> 
                 <h1 class="card-subtitle">${usuario.last_name}</h1>
-                <p class="card-text">id: ${usuario.id}</p>
+                <p class="card-text">ID: ${usuario.id}</p>
                 <p class="card-text">Correo: ${usuario.email}</p>
             </div>
         </div>
-    `;
+        `;
         cardsSpan.innerHTML += newCard;
     });
 };
 const imprimirCards = (usuarios, contenedorId) => {
-    setTimeout(() => rellenarCards(usuarios, contenedorId), 1000);
+    setTimeout(() => {
+        const loader = document.getElementById("loader");
+        loader.style.display = 'none';
+        rellenarCards(usuarios, contenedorId);
+    }, 1000);
 };
 const datos = async (url, contenedorId) => {
+    const loader = document.getElementById("loader");
+    loader.style.display = 'block';
     const resolve = await fetch(url);
     const users = await resolve.json();
     imprimirCards(users.data, contenedorId);
 };
+datos("https://reqres.in/api/users?page=1", "cards");
